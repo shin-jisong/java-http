49947b5fa0b5b867b68ee93dc5beb3a1a2712061
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-01-06 14:12:09 +0900

    feat: Http11ResponseBuilder 구현

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/Http11ResponseBuilder.java
@@ -0,0 +1,12 @@
+package org.apache.coyote.http11.response;
+
+public class Http11ResponseBuilder {
+
+    private static final String HTTP_11 = "HTTP/1.1";
+
+    public static Http11Response build(StatusCode statusCode, ContentType contentType, String body) {
+        ResponseStatusLine statusLine = new ResponseStatusLine(HTTP_11, statusCode);
+        ResponseHeader header = new ResponseHeader(contentType, body.length());
+        return new Http11Response(statusLine, header, body);
+    }
+}
