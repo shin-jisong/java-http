0c2947eb36ea842f65d6d39c2ce766352af6a065
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-01-06 18:43:42 +0900

    feat(ContentType): extension 도입

--- tomcat/src/main/java/org/apache/coyote/http11/response/ContentType.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/ContentType.java
@@ -1,14 +1,30 @@
 package org.apache.coyote.http11.response;
 
+import java.util.Arrays;
+import java.util.Set;
+
 public enum ContentType {
-    TEXT_HTML_UTF8("text/html", "charset=utf-8");
+
+    TEXT_HTML_UTF8("text/html", "charset=utf-8", "html"),
+    TEXT_CSS("text/css", null, "css"),
+    TEXT_JAVASCRIPT("text/javascript", null, "js")
+    ;
 
     private final String mediaType;
     private final String parameter;
+    private final String extension;
 
-    ContentType(String mediaType, String parameter) {
+    ContentType(String mediaType, String parameter, String extension) {
         this.mediaType = mediaType;
         this.parameter = parameter;
+        this.extension = extension;
+    }
+
+    public static ContentType fromExtension(String extension) {
+        return Arrays.stream(values())
+                .filter(contentType -> contentType.extension.equalsIgnoreCase(extension))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown extension: " + extension));
     }
 
     public String getMediaType() {
@@ -17,5 +33,9 @@ public enum ContentType {
 
     public String getParameter() {
         return parameter;
+    }
+
+    public String getExtension() {
+        return extension;
     }
 }
