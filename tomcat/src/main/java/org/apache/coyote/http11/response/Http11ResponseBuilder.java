b234c42dbdc06edae011fd0ab8836e0d83e2c4ef
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-01-21 18:34:51 +0900

    feat(ResponseBuilder): 쿠키 부분 추가

--- tomcat/src/main/java/org/apache/coyote/http11/response/Http11ResponseBuilder.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/Http11ResponseBuilder.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11.response;
 
+import org.apache.coyote.http11.cookie.HttpCookie;
+
 public class Http11ResponseBuilder {
 
     private static final String HTTP_11 = "HTTP/1.1";
 
-    public static Http11Response build(StatusCode statusCode, ContentType contentType, String body) {
+    public static Http11Response build(StatusCode statusCode, ContentType contentType, HttpCookie cookie, String body) {
         ResponseStatusLine statusLine = new ResponseStatusLine(HTTP_11, statusCode);
-        ResponseHeader header = new ResponseHeader(contentType, body.getBytes().length);
+        ResponseHeader header = new ResponseHeader(contentType, body.getBytes().length, cookie);
         return new Http11Response(statusLine, header, body);
+    }
+
+    public static Http11Response build(StatusCode statusCode, ContentType contentType, String body) {
+        return build(statusCode, contentType, null, body);
     }
 }
