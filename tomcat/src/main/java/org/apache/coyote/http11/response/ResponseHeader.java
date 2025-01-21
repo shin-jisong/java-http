20c0c14804fc93e3541c91c93326772f7a53ad16
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-01-21 18:34:32 +0900

    feat(ResponseHeader): 쿠키 부분 추가

--- tomcat/src/main/java/org/apache/coyote/http11/response/ResponseHeader.java
+++ tomcat/src/main/java/org/apache/coyote/http11/response/ResponseHeader.java
@@ -1,13 +1,18 @@
 package org.apache.coyote.http11.response;
 
+import jakarta.servlet.http.Cookie;
+import org.apache.coyote.http11.cookie.HttpCookie;
+
 public class ResponseHeader {
 
     private final ContentType contentType;
     private final long contentLength;
+    private final HttpCookie cookie;
 
-    public ResponseHeader(ContentType contentType, long contentLength) {
+    public ResponseHeader(ContentType contentType, long contentLength, HttpCookie cookie) {
         this.contentType = contentType;
         this.contentLength = contentLength;
+        this.cookie = cookie;
     }
 
     public ContentType getContentType() {
@@ -16,5 +21,9 @@ public class ResponseHeader {
 
     public long getContentLength() {
         return contentLength;
+    }
+
+    public HttpCookie getCookie() {
+        return cookie;
     }
 }
